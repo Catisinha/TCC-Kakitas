330c285bf7bbcc8b3caa313ae3a86a41a94e666b
Catisinha <luacoutinhor@gmail.com>
2024-08-07 10:56:27 -0300

    Criando o mvc do zero

---
+++ MAContabilidade/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MAContabilidade.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ MAContabilidade/Models/Servicos.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TCCKakitas.Models;
+
+[Table("Servicos")]
+public class Servicos
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+        
+    [Required(ErrorMessage = "Por favor, informe o nome do serviço")]
+    [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "Por favor, informe a descrição do serviço")]
+    [StringLength(200, ErrorMessage = "A descrição deve possuir no máximo 200 caracteres")]
+
+    public string Descricao { get; set; }
+}
