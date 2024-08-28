9726d1b279032c709ed26b688096a2e36844c37e
Catisinha <luacoutinhor@gmail.com>
2024-08-28 10:57:43 -0300

    terminar css do obrigada.css e linkar a página obrigada.cshtml e login.cshtml no index.cshtml, e criar o botão admin na página pricipal

---
+++ MAContabilidade/wwwroot/js/login.js
@@ -0,0 +1,9 @@
+var formSignin = document.querySelector('#signin')
+var btnColor = document.querySelector('.btnColor')
+
+document.querySelector('#btnSignin')
+  .addEventListener('click', () => {
+    formSignin.style.left = "25px"
+    formSignup.style.left = "450px"
+    btnColor.style.left = "0px"
+})
