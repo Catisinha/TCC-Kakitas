4802c58df117c37d3b8c68a0b7fdd155a880799a
Catisinha <luacoutinhor@gmail.com>
2024-09-20 11:24:59 -0300

    terminar de fazer o Translate da pasta Helpers

---
+++ MAContabilidade/Helpers/TranslateIdentityErrors.cs
@@ -0,0 +1,13 @@
+namespace MAContabilidade.Helpers;
+
+public static class TranslateIdentityErrors
+{
+    public static string TranslateErrorMessage(string codeError)
+    {
+        string message = codeError switch
+        {
+            "DefaultError" => "Ocorreu um erro desconhecido.",
+            "ConcurrencyFailure" => "Falha de concorrência otimista, o objeto foi modificado.",
+        }
+    }
+}
--- MAContabilidade/ViewModels/LoginVM.cs
+++ MAContabilidade/ViewModels/LoginVM.cs
@@ -4,14 +4,17 @@ namespace MAContabilidade.ViewModels;
 
 public class LoginVM
 {
-    [Display(Name = "Email ou Nome de Usuário")]
+    [Display(Name = "Email ou Nome de Usuário", Prompt = "Informe seu Email ou Nome de Usuário")]
     [Required(ErrorMessage = "Por favor, infome seu email ou nome de usuário")]
     public string Email { get; set; }
+    
     [DataType(DataType.Password)]
-    [Display(Name = "Senha de Acesso")]
+    [Display(Name = "Senha de Acesso", Prompt = "********")]
     [Required(ErrorMessage = "Por favor, infome sua senha")]
     public string Senha { get; set; }
+
     [Display(Name = "Manter Conectado?")]
     public bool Lembrar { get; set; } = false;
+
     public string UrlRetorno { get; set; }
 }
